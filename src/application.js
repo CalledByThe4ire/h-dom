4aac57a6c999024aa57df94b5e5b58b0e851fed5
Andrey Karamnoff <karamnoff@gmail.com>
2019-05-03 00:12:16 +0300

    add hexlet' solution for 01-progress-challenge

--- src/application.js
+++ src/application.js
@@ -1,5 +1,17 @@
 import '@babel/polyfill';
+import { watch as controller } from 'melanke-watchjs';
 
 // BEGIN (write your solution here)
-
+export default () => {
+  const element = document.querySelector('progress');
+  let counter = 0;
+  const handler = () => {
+    counter += 1;
+    element.setAttribute('value', counter);
+    if (counter !== 100) {
+      setTimeout(handler, 1000);
+    }
+  };
+  setTimeout(handler, 1000);
+};
 // END

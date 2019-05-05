f981132f8a9b8946d41519c11495bbc6e268b9ef
Andrey Karamnoff <karamnoff@gmail.com>
2019-05-05 21:34:07 +0300

    add hexlet' solution for 02-modal-challenge

--- src/application.js
+++ src/application.js
@@ -1,53 +1,22 @@
 import '@babel/polyfill';
 
+const openHandler = (modal) => {
+  modal.classList.add('show');
+  modal.style.display = 'block'; // eslint-disable-line
+};
+
+const closeHandler = (modal) => {
+  modal.classList.remove('show');
+  modal.style.display = 'none'; // eslint-disable-line
+};
+
 export default () => {
-  const model = {
-    id: null,
-    active: null,
-  };
-
-  const updateView = () => {
-    const [activeModal] = [...document.querySelectorAll('.modal')].filter(
-      modal => modal.id === proxiedModel.id, // eslint-disable-line no-use-before-define
-    );
-
-    if (activeModal) {
-      if (proxiedModel.active) { // eslint-disable-line no-use-before-define
-        activeModal.classList.add('show');
-        activeModal.style.display = 'block';
-      } else {
-        activeModal.classList.remove('show');
-        activeModal.style.display = 'none';
-      }
-    }
-  };
-
-  const handler = {
-    set: (target, prop, newValue) => {
-      if (prop in target) {
-        target[prop] = newValue; // eslint-disable-line no-param-reassign
-        updateView();
-        return true;
-      }
-      return false;
-    },
-  };
-
-  const proxiedModel = new Proxy(model, handler);
-
-  const updateModel = ({ target }) => {
-    if (target.dataset.toggle) {
-      proxiedModel.id = target.dataset.target.slice(1);
-      proxiedModel.active = true;
-    } else {
-      proxiedModel.id = target.closest('.modal').id;
-      proxiedModel.active = false;
-    }
-  };
-
-  [
-    ...document.querySelectorAll(
-      'button[data-toggle="modal"], button[data-dismiss="modal"]',
-    ),
-  ].forEach(btn => btn.addEventListener('click', updateModel));
+  const buttons = document.querySelectorAll('[data-toggle="modal"]');
+  buttons.forEach((element) => {
+    const id = element.dataset.target;
+    const modal = document.querySelector(id);
+    element.addEventListener('click', openHandler.bind(null, modal));
+    const closeButton = modal.querySelector('[data-dismiss="modal"]');
+    closeButton.addEventListener('click', closeHandler.bind(null, modal));
+  });
 };

9098e979e768437536a5e1c92340dfffabab0f8c
Andrey Karamnoff <karamnoff@gmail.com>
2019-05-05 22:01:01 +0300

    add hexlet' solution for 05-dom-hierarchy

--- src/search.js
+++ src/search.js
@@ -1,7 +1,11 @@
 import '@babel/polyfill';
 
 const search = (doc, tag) => {
-  //
+  const coll = [...doc.children];
+  const initAcc = coll.filter(e => e.tagName.toLowerCase() === tag);
+  return coll.reduce((acc, child) => {
+    return [...acc, ...search(child, tag)];
+  }, initAcc);
 };
 
 export default search;

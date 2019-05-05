71cd74920904e945eb3bdb01fb3d3bbab2cba636
Andrey Karamnoff <karamnoff@gmail.com>
2019-05-05 22:38:49 +0300

    add solution for 16-jquery

--- src/application.js
+++ src/application.js
@@ -3,5 +3,34 @@ import _ from 'lodash';
 import $ from 'jquery';
 
 export default () => {
-  //
+  const clickHandler = function() {
+    const $this = $(this);
+    const $itemsContainer = $this
+      .closest('[data-ride="carousel"]')
+      .find('.carousel-inner');
+    const $items = $('.carousel-item', $itemsContainer);
+    const $activeItem = $items.filter((index, item) =>
+      $(item).hasClass('active')
+    );
+
+    switch ($this.attr('data-slide')) {
+      case 'prev':
+        const $prevItem =
+          $activeItem.prev().length === 0 ? $items.last() : $activeItem.prev();
+        $activeItem.removeClass('active');
+        $prevItem.addClass('active');
+        break;
+      case 'next':
+        const $nextItem =
+          $activeItem.next().length === 0 ? $items.first() : $activeItem.next();
+        $activeItem.removeClass('active');
+        $nextItem.addClass('active');
+        break;
+    }
+  };
+
+  const $controls = $('[data-slide]');
+  $controls.each((index, control) => {
+    $(control).on('click', clickHandler);
+  });
 };

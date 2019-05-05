9a61990b750e155a840ca8c810794e47d8eb10a2
Andrey Karamnoff <karamnoff@gmail.com>
2019-05-05 22:25:27 +0300

    add solution for 13-event-stages

--- src/application.js
+++ src/application.js
@@ -22,5 +22,70 @@ const generatePlayingField = () => {
 };
 
 export default () => {
-  //
+  const makeStep = (c) => {
+    // cell
+    const cell = c;
+    const { cellIndex } = cell;
+
+    // row
+    const row = cell.parentElement;
+    const { rowIndex } = row;
+
+    // table's body
+    const rows = row.closest('tbody').rows;
+    const { cells } = row;
+
+    // cell' siblings
+    let leftSiblingCell = null;
+    let rightSiblingCell = null;
+    let topSiblingCell = null;
+    let bottomSiblingCell = null;
+
+    if (cells[cellIndex - 1]) {
+      leftSiblingCell = rows[rowIndex].cells[cellIndex - 1];
+    }
+
+    if (cells[cellIndex + 1]) {
+      rightSiblingCell = rows[rowIndex].cells[cellIndex + 1];
+    }
+
+    if (cells[rowIndex - 1]) {
+      topSiblingCell = rows[rowIndex - 1].cells[cellIndex];
+    }
+
+    if (cells[rowIndex + 1]) {
+      bottomSiblingCell = rows[rowIndex + 1].cells[cellIndex];
+    }
+
+    const isActive = cell => cell.classList.contains('table-active');
+    const setData = (currentCell, siblingCell) => {
+      siblingCell.classList.remove('table-active');
+      siblingCell.textContent = currentCell.textContent;
+
+      currentCell.textContent = '';
+      currentCell.classList.add('table-active');
+    };
+
+    if (rightSiblingCell && isActive(rightSiblingCell)) {
+      setData(cell, rightSiblingCell);
+    }
+
+    if (leftSiblingCell && isActive(leftSiblingCell)) {
+      setData(cell, leftSiblingCell);
+    }
+
+    if (topSiblingCell && isActive(topSiblingCell)) {
+      setData(cell, topSiblingCell);
+    }
+
+    if (bottomSiblingCell && isActive(bottomSiblingCell)) {
+      setData(cell, bottomSiblingCell);
+    }
+  };
+  const clickHandler = (evt) => {
+    const { target } = evt;
+    makeStep(target);
+  };
+  document.querySelector('.gem-puzzle').append(generatePlayingField());
+  document.addEventListener('click', clickHandler);
 };

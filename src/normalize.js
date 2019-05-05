35f396b3e4666990b7f67b24fc19a37b8af60282
Andrey Karamnoff <karamnoff@gmail.com>
2019-05-05 22:09:42 +0300

    add hexlet' solution for 10-element-manipulating

--- src/normalize.js
+++ src/normalize.js
@@ -1,15 +1,9 @@
 import '@babel/polyfill';
 import { camelCase } from 'lodash';
 
-export default () => {
-  const tagsList = document.body.getElementsByTagName('*');
-  [...tagsList].forEach((tag) => {
-    const classList = tag.getAttribute('class');
-    const camelizedClassList = classList
-      .split(' ')
-      .map(cls => camelCase(cls))
-      .join(' ');
-
-    tag.className = camelizedClassList;
-  });
+export default (doc) => {
+  for (const element of doc.body.getElementsByTagName('*')) { // eslint-disable-line
+    const process = item => element.classList.replace(item, camelCase(item));
+    element.classList.forEach(process);
+  }
 };

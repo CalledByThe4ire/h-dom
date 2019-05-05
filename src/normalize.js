c9a4bd8385346167510290d37a6cce58629c70b4
Andrey Karamnoff <karamnoff@gmail.com>
2019-05-05 22:08:51 +0300

    add solution for 10-element-manipulating

--- src/normalize.js
+++ src/normalize.js
@@ -2,5 +2,14 @@ import '@babel/polyfill';
 import { camelCase } from 'lodash';
 
 export default () => {
-  //
+  const tagsList = document.body.getElementsByTagName('*');
+  [...tagsList].forEach((tag) => {
+    const classList = tag.getAttribute('class');
+    const camelizedClassList = classList
+      .split(' ')
+      .map(cls => camelCase(cls))
+      .join(' ');
+
+    tag.className = camelizedClassList;
+  });
 };
